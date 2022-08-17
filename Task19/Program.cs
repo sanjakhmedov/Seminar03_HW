92ffcaf8a484f27ed4acd13bb453c3dd42c7bdec
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-17 19:22:03 +0500

    task 19 is done

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,4 +7,20 @@
 // 23432 -> да
 
 Console.Write("Enter the 5 digit number: ");
-int num = Convert.IntTo32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n0 = n / 10000;
+int n1 = n / 1000 % 10;
+int n2 = n / 100 % 10;
+int n3 = n / 10 % 10;
+int n4 = n % 10;
+Console.WriteLine(n4);
+
+if (n0 == n4 && n1 == n3) 
+{
+    Console.WriteLine($"Number '{n}' is Palindrome");
+}
+else 
+{
+    Console.WriteLine($"Number '{n}' is NOT Palindrome");
+}
